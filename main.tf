740ff7d41405b4b03011243ff0445560a573297f
olga-matusik <olga.matusik@cloudreach.com>
2022-09-14 16:59:29 +0200

    creating new backend for talent-academy

---
+++ main.tf
@@ -0,0 +1,40 @@
+#resource "resource_type" "resource_name" for terraform
+resource "aws_s3_bucket" "ta_backend_bucket" {
+  #bucket = "bucket_name_globally_unique" for aws
+  bucket = "ta-terraform-tfstates-727250514989"
+
+  #meta-data
+  lifecycle {
+      prevent_destroy = true
+    }
+
+    tags = {
+        Name = "ta-terraform-tfstates-727250514989"
+        Environment = "Test"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "version_my_bucket" {
+    #redirect to get the name of the bucket we want to versioning
+  bucket = aws_s3_bucket.ta_backend_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock_tbl" {
+  name           = "terraform-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags           = {
+    Name = "terraform-lock"
+  }
+}
